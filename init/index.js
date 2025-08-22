b9e6144a8ea3c9c89c7ba359d89409e99a77ac81
Prince Thummar <princethummar199@gmail.com>
2025-08-22 22:24:12 +0530

    Adding Authorization

--- init/index.js
+++ init/index.js
@@ -17,8 +17,10 @@ async function main(){
 
 const initDB = async()=>{
     await Listing.deleteMany({})
+    initData.data = initData.data.map((obj)=> { return {...obj,owner:'68a6e274542b44622ea1302a' }})
     Listing.insertMany(initData.data);
     console.log(`Data is Intilized`)
 }
+
 
 initDB()
